1207660b0458852c5e964f9cd4db8ac5e74760b8
Rome Bell <romebell5@gmail.com>
2021-09-18 01:27:51 -0700

    add: comment

--- controllers/advertisement.js
+++ controllers/advertisement.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../middleware/isLoggedIn");
-const { Advertisement, User } = require("../models");
+const { Advertisement, User, Comment } = require("../models");
 
 router.get("/", isLoggedIn, async (req, res) => {
   try {
@@ -52,11 +52,32 @@ router.get("/:idx", isLoggedIn, async (req, res) => {
 
     console.log(cleanAd);
 
-    res.render("ads/show", { ad: cleanAd });
+    let rawComments = await Comment.findAll({
+      include: [User]
+    });
+    let comments = rawComments.map(c => c.toJSON());
+
+    res.render("ads/show", { ad: cleanAd, comments });
   } catch (error) {
     console.log(error);
     res.render("404");
   }
 });
+
+router.post('/comments', isLoggedIn, async (req, res) => {
+  try {
+    let newComment = await Comment.create({
+      title: req.body.title,
+      content: req.body.content,
+      userId: req.user.id
+    })
+    let comment = newComment.toJSON();
+    console.log('NEW COMMENT AT 1:18AM', comment);
+    res.redirect(`/ads/${req.body.adId}`);
+  } catch (error) {
+    console.log(error);
+    res.redirect(`/ads/${req.body.adId}`);
+  }
+})
 
 module.exports = router;
